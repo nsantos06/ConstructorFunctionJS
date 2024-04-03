93b0058b008a4b333ede408b19b1481f3365399b
Nicollas dos Santos <nicollasmk9@gmail.com>
2024-04-02 21:43:37 -0300

    Mudado o local das funcoes

--- index.js
+++ index.js
@@ -5,6 +5,28 @@ function VendaVeiculos(veiculo, modelo, marca, ano, preco){
     this.ano = ano;
     this.preco = preco;
 } 
+function imprimirCarroLista(){
+    for(i of carroLista){
+        console.log(i.veiculo, i.modelo, i.marca, i.ano, i.preco);
+    }
+}
+function imprimirMotoLista(){
+    for(i of motoLista){
+        console.log(i.veiculo, i.modelo, i.marca, i.ano, i.preco);
+    }
+}
+function listarVeiculoAno(){
+    for(i of carroLista){
+        if(i.ano === 2018){
+            console.log(i.veiculo, i.modelo, i.marca, i.ano, i.preco);
+        }
+    }
+    for(i of motoLista){
+        if(i.ano === 2018){
+            console.log(i.veiculo, i.modelo, i.marca, i.ano, i.preco);
+        }
+    }
+}
 
 const carro1 = new VendaVeiculos('Carro', 'Fiesta', 'Ford', 2019, 30000);
 const carro2 = new VendaVeiculos('Carro', 'Focus', 'Ford', 2018, 25000);
@@ -18,30 +40,6 @@ const moto3 = new VendaVeiculos('Moto', 'Honda', 'CBR 400', 2017, 10000);
 let carroLista = [carro1, carro2, carro3, carro4, carro5];
 let motoLista = [moto1, moto2, moto3];
 
-function imprimirCarroLista(){
-    for(i of carroLista){
-        console.log(i.veiculo, i.modelo, i.marca, i.ano, i.preco);
-    }
-}
-
-function imprimirMotoLista(){
-    for(i of motoLista){
-        console.log(i.veiculo, i.modelo, i.marca, i.ano, i.preco);
-    }
-}
-
-function listarVeiculoAno(){
-    for(i of carroLista){
-        if(i.ano === 2018){
-            console.log(i.veiculo, i.modelo, i.marca, i.ano, i.preco);
-        }
-    }
-    for(i of motoLista){
-        if(i.ano === 2018){
-            console.log(i.veiculo, i.modelo, i.marca, i.ano, i.preco);
-        }
-    }
-}
 imprimirCarroLista();
 imprimirMotoLista();
 listarVeiculoAno();
